7f4d687baa98bbc01fd56930b3e33fcaaef5b091
Leftyx <171024+Leftyx@users.noreply.github.com>
2022-03-06 11:32:33 +0000

    commenting stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,7 +31,7 @@ export class AppComponent implements OnInit {
       switchMap((currentPage) =>
         this.usersService.getApiPaginatedUsers(currentPage)
       ),
-      // map((result: UserResult) => result.results || [])
+      // // map((result: UserResult) => result.results || [])
     );
   }
 
